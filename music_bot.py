2737d87bd56ec8f27dd54dc22a8417de96cb6f27
doniyornazarov520-lang <doniyornazarov520@gmail.com>
2026-09-05 21:37:45 +0500

    Update music_bot.py

--- music_bot.py
+++ music_bot.py
@@ -1,3 +1,22 @@
+import os
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route('/')
+def home():
+    return "Bot faol ishlamoqda!"
+
+def run():
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+keep_alive()
 from datetime import datetime
 import json
 import os
